cf2a7b30d6f0febe714bfe8c2d997eb10a3dbf76
Frank Thompson <frankthompson@fb.com>
2019-02-05 18:16:37 -0800

    more compiler stuff

--- haskell/Compiler/Compiler.hs
+++ haskell/Compiler/Compiler.hs
@@ -4,34 +4,58 @@ module Compiler.Compiler
 
 import Analyzer.Types
 import Compiler.SymTab
+import Control.Monad.State
 import Data.List (foldl')
 import VM.Types
+
+type Context = State (String, Int)
 
 compile :: AClass -> [Command]
 compile (AClass cName varDecs subDecs) = concat $ map (compileSubDec cName symTab) subDecs
   where symTab = foldl' addClassVarDec [] varDecs
 
+fullName :: String -> String -> String
+fullName cName sName = cName ++ "." ++ sName
+
+getLbl :: Context String
+getLbl = do
+  (fullName, lblCount) <- get
+  put (fullName, lblCount + 1)
+  return $ fullName ++ "##" ++ (show lblCount)
+
+compileStatement :: AStatement -> Context [Command]
+compileStatement (ALet var mArray expr) = error "not implemented"
+compileStatement (ADo call) = error "not implemented"
+compileStatement (AReturn mExpr) = error "not implemented"
+compileStatement (AIf expr ifStmts mElseStmts) = error "not implemented"
+compileStatement (AWhile expr stmts) = error "not implemented"
+
 compileSubDec :: String -> SymTab -> ASubroutineDec -> [Command]
-compileSubDec cName cTable dec@(ASubroutineDec _ ret sName params body) = error "not implemented"
-  where symTab = addSubDec cName cTable dec
+compileSubDec cName cTable dec@(ASubroutineDec subKind ret sName params (ASubroutineBody localDecs statements)) =
+  (CFun $ Fun (fullName cName sName) nLocals):compiledBody
+  where
+    symTab = addSubDec cName cTable dec
+    nLocalsOfDec (AVarDec _ names) = length names
+    nLocals = sum $ map nLocalsOfDec localDecs
+    compiledBody = []
 
 addMany :: (SymTab -> String -> SymTab) -> [String] -> SymTab
 addMany update strings = foldl' update [] strings
 
 addClassVarDec :: SymTab -> AClassVarDec -> SymTab
-addClassVarDec table (AStatic theType names) = addMany (add VStatic theType) names
-addClassVarDec table (AField theType names) = addMany (add VField theType) names
+addClassVarDec table (AStatic theType names) = addMany (addSym VStatic theType) names
+addClassVarDec table (AField theType names) = addMany (addSym VField theType) names
 
 addVarDec :: SymTab -> AVarDec -> SymTab
-addVarDec table (AVarDec theType names) = addMany (add VLocal theType) names
+addVarDec table (AVarDec theType names) = addMany (addSym VLocal theType) names
 
 addParam :: SymTab -> AParameter -> SymTab
-addParam table (AParameter theType name) = add VArgument theType table name
+addParam table (AParameter theType name) = addSym VArgument theType table name
 
 addSubDec :: String -> SymTab -> ASubroutineDec -> SymTab
 addSubDec cName table (ASubroutineDec subKind _ _ params (ASubroutineBody localDecs _)) = withBody
   where
-    withThis = if subKind == AMethod then add VArgument (AClassName cName) table "this" else table
+    withThis = if subKind == AMethod then addSym VArgument (AClassName cName) table "this" else table
     withParams = foldl' addParam withThis params
     withBody = foldl' addVarDec withParams localDecs
 
--- haskell/Compiler/SymTab.hs
+++ haskell/Compiler/SymTab.hs
@@ -11,13 +11,13 @@ data SymVar = SymVar String AType VarKind Int
 type SymTab = [SymVar]
 
 -- odd order is to make it easy to use with foldl'
-add :: VarKind -> AType -> SymTab -> String -> SymTab
-add kind theType table name = (SymVar name theType kind (nextIndex kind table)):table
+addSym :: VarKind -> AType -> SymTab -> String -> SymTab
+addSym kind theType table name = (SymVar name theType kind (nextIndex kind table)):table
   where
     nextIndex kind [] = 0
     nextIndex kind ((SymVar _ _ k idx):rest) =
       if k == kind then idx + 1 else nextIndex kind rest
 
-get :: SymTab -> String -> Maybe SymVar
-get table name = find (hasName name) table
+getSym :: SymTab -> String -> Maybe SymVar
+getSym table name = find (hasName name) table
   where hasName name (SymVar n _ _ _) = n == name
